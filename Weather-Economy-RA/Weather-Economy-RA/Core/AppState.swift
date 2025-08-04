52ba580e735079d760475de781c3f6040f93759f
mitchsaah <mitch.saah@student.ehb.be>
2025-08-04 19:09:39 +0200

    Added a user initialization with didSet logging

--- Weather-Economy-RA/Weather-Economy-RA/Core/AppState.swift
+++ Weather-Economy-RA/Weather-Economy-RA/Core/AppState.swift
@@ -5,5 +5,9 @@ import Combine
 final class AppState: ObservableObject {
     static let shared = AppState()
     
-    @Published var user: User?
+    @Published var user: User? = Auth.auth().currentUser {
+        didSet {
+            print("[AppState] user changed", user?.uid ?? "nil")
+        }
+    }
 }
