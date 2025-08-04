b7600340ead1acac74b34589ffb84f4201730686
mitchsaah <mitch.saah@student.ehb.be>
2025-08-04 11:05:28 +0200

    Added the Firebase initialization code

--- Weather-Economy-RA/Weather-Economy-RA/Weather_Economy_RAApp.swift
+++ Weather-Economy-RA/Weather-Economy-RA/Weather_Economy_RAApp.swift
@@ -1,14 +1,20 @@
-//
-//  Weather_Economy_RAApp.swift
-//  Weather-Economy-RA
-//
-//  Created by Mitch Saah on 04/08/2025.
-//
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct Weather_Economy_RAApp: App {
+    // Registers app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
