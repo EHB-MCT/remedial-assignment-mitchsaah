9fd8e175c541c0bb76ece140faefb9e6029c45ed
mitchsaah <mitch.saah@student.ehb.be>
2025-08-04 09:36:04 +0200

    Added Xcode project

---
+++ Weather-Economy-RA/Weather-Economy-RA/Weather_Economy_RAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Economy_RAApp.swift
+//  Weather-Economy-RA
+//
+//  Created by Mitch Saah on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Economy_RAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
