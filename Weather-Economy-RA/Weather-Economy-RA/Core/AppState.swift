a6ce476e860cff60716383ec354fcdb4bfb1d9a1
mitchsaah <mitch.saah@student.ehb.be>
2025-08-05 09:29:21 +0200

    Added a handle property

--- Weather-Economy-RA/Weather-Economy-RA/Core/AppState.swift
+++ Weather-Economy-RA/Weather-Economy-RA/Core/AppState.swift
@@ -10,4 +10,19 @@ final class AppState: ObservableObject {
             print("[AppState] user changed", user?.uid ?? "nil")
         }
     }
+    
+    private var handle: AuthStateDidChangeListenerHandle?
+    
+    private init() {
+        handle = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            self?.user = user
+        }
+    }
+    
+    deinit {
+        if let h = handle {
+            Auth.auth().removeStateDidChangeListener(h)
+        }
+    }
+
 }
