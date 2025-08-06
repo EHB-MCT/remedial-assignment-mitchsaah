23670553bd91a1c4dacb033f99448e4a3a1509ea
mitchsaah <mitch.saah@student.ehb.be>
2025-08-06 14:07:44 +0200

    Added properties for email/password login

--- Weather-Economy-RA/Weather-Economy-RA/ViewModel/Auth/AuthViewModel.swift
+++ Weather-Economy-RA/Weather-Economy-RA/ViewModel/Auth/AuthViewModel.swift
@@ -2,5 +2,8 @@ import Foundation
 import Combine
 
 final class AuthViewModel: ObservableObject {
-    
+    @Published var email = ""
+    @Published var password = ""
+    @Published var isLogin = true
+    @Published var errorMessage: String?
 }
