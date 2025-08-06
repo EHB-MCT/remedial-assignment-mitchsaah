75a96e60b0bac2d33ed5d1351b6d5ffd40d12e6d
mitchsaah <mitch.saah@student.ehb.be>
2025-08-06 12:34:21 +0200

    Added a signUp function

--- Weather-Economy-RA/Weather-Economy-RA/Service/Auth/AuthService.swift
+++ Weather-Economy-RA/Weather-Economy-RA/Service/Auth/AuthService.swift
@@ -6,3 +6,11 @@ final class AuthService {
     static let shared = AuthService()
     private init () {}
 }
+
+extension AuthService {
+    func signUp(email: String, password: String, completion: @escaping (Error?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { _, error in
+            completion(error)
+        }
+    }
+}
