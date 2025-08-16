1873c4d1aec1d7aa29ba0254c99881cde373ad7b
mitchsaah <mitch.saah@student.ehb.be>
2025-08-16 14:15:08 +0200

    Added baseURL and Info file apiKey accessor

--- Weather-Economy-RA/Weather-Economy-RA/Service/OpenWeather/WeatherService.swift
+++ Weather-Economy-RA/Weather-Economy-RA/Service/OpenWeather/WeatherService.swift
@@ -3,4 +3,12 @@ import Alamofire
 
 final class WeatherService {
     static let shared = WeatherService()
+    private let baseURL = "https://api.openweathermap.org/data/3.0/onecall"
+
+    private var apiKey: String {
+        guard let key = Bundle.main.infoDictionary?["OpenWeatherAPIKey"] as? String, !key.isEmpty else {
+            fatalError("OpenWeatherAPIKey missing")
+        }
+        return key
+    }
 }
