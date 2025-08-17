70616af11788cf42291dfd4b1cf778f8581933f2
mitchsaah <mitch.saah@student.ehb.be>
2025-08-17 11:24:13 +0200

    Added StatCard inputs

--- Weather-Economy-RA/Weather-Economy-RA/View/Dashboard/Components/StatCard.swift
+++ Weather-Economy-RA/Weather-Economy-RA/View/Dashboard/Components/StatCard.swift
@@ -1,6 +1,11 @@
 import SwiftUI
 
 struct StatCard: View {
+    let title: String
+    let value: String
+    let subtitle: String?
+    let systemImage: String
+    
     var body: some View {
         EmptyView()
     }
