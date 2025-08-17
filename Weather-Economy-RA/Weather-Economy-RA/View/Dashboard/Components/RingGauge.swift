15dd41e02cbdc4a7f70f0bf4f064640ab7bfe057
mitchsaah <mitch.saah@student.ehb.be>
2025-08-17 09:34:14 +0200

    Created RingGauge with inputs

---
+++ Weather-Economy-RA/Weather-Economy-RA/View/Dashboard/Components/RingGauge.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct RingGauge: View {
+    let progress: Double
+    let title: String
+    let valueText: String
+    var lineWidth: CGFloat = 14
+
+    var body: some View {
+        EmptyView()
+    }
+}
