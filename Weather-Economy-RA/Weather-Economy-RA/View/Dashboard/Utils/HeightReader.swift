1dafa6b3fa0e1ecf07f92269b75b652b6fc0de6f
mitchsaah <mitch.saah@student.ehb.be>
2025-08-17 02:11:11 +0200

    Added HeightReader that publishes its geo height

--- Weather-Economy-RA/Weather-Economy-RA/View/Dashboard/Utils/HeightReader.swift
+++ Weather-Economy-RA/Weather-Economy-RA/View/Dashboard/Utils/HeightReader.swift
@@ -1,5 +1,14 @@
 import SwiftUI
 
+struct HeightReader: View {
+    var body: some View {
+        GeometryReader { proxy in
+            Color.clear
+                .preference(key: HeightKey.self, value: proxy.size.height)
+        }
+    }
+}
+
 struct HeightKey: PreferenceKey {
     static var defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
