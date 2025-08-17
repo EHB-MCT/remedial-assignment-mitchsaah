d12b33b4a9c84a4acf791f88ce90034e67e26703
mitchsaah <mitch.saah@student.ehb.be>
2025-08-17 02:05:13 +0200

    Added .reduce for largest measured height

--- Weather-Economy-RA/Weather-Economy-RA/View/Dashboard/Utils/HeightReader.swift
+++ Weather-Economy-RA/Weather-Economy-RA/View/Dashboard/Utils/HeightReader.swift
@@ -2,5 +2,7 @@ import SwiftUI
 
 struct HeightKey: PreferenceKey {
     static var defaultValue: CGFloat = 0
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) { }
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = max(value, nextValue())
+    }
 }
