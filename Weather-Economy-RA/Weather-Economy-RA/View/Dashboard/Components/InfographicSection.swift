d187660a667e39c968efb32961463527f8ae1979
mitchsaah <mitch.saah@student.ehb.be>
2025-08-17 13:21:27 +0200

    Created InfographicSection
    With inputs and refresh callback

---
+++ Weather-Economy-RA/Weather-Economy-RA/View/Dashboard/Components/InfographicSection.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct InfographicSection: View {
+    let uvi: Double?
+    let clouds: Int?
+    let condition: String?
+    let rain1h: Double?
+    let estKWh: Double?
+    let estEUR: Double?
+    let onRefresh: () -> Void
+
+    var body: some View {
+        EmptyView()
+    }
+}
